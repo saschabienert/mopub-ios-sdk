c8fd37c321931bb3d5942bdaf9a31dab2d166540
Maximilian Tagher <feedback.tagher@gmail.com>
2014-03-11 16:47:53 -0700

    Don't require xcpretty
    Conflicts:
    	runUnitTests.sh

--- runUnitTests.sh
+++ runUnitTests.sh
@@ -2,4 +2,12 @@ OS_VERSION=${1-"7.1"} # Default to 7.1, which Xcode 5.1 has installed by default
 
 echo "Using $OS_VERSION"
 
-xcodebuild -workspace Heyzap/Heyzap.xcworkspace -scheme Ads -sdk iphonesimulator$OS_VERSION -destination platform='iOS Simulator',OS=$OS_VERSION,name='iPhone Retina (4-inch)' build test | xcpretty -c; exit ${PIPESTATUS[0]}
+if hash xcpretty 2>/dev/null; then
+	TEST_COMMAND="xcpretty -c"
+else
+	echo "Not using xcpretty. Run `gem install xcpretty` to get pretty printed xcodebuild output"
+	TEST_COMMAND="tee"
+fi
+
+xcodebuild -workspace Heyzap/Heyzap.xcworkspace -scheme Ads -sdk iphonesimulator$OS_VERSION -destination platform='iOS Simulator',OS=$OS_VERSION,name='iPhone Retina (4-inch)' build test | eval $TEST_COMMAND; exit ${PIPESTATUS[0]}
+
