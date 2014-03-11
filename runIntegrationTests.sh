799361ca562413ce874aac91c9ccc31b1a943c33
Maximilian Tagher <feedback.tagher@gmail.com>
2014-03-11 13:30:38 -0700

    Integration tests script for both travis and locally

--- runIntegrationTests.sh
+++ runIntegrationTests.sh
@@ -1,1 +1,10 @@
-"Heyzap/Pods/Subliminal/Supporting Files/CI/subliminal-test" -workspace "Heyzap/Heyzap.xcworkspace" -sim_device 'iPhone Retina (3.5-inch)' -output integrationTestResults -login_password "j4K7CK4oM49ZA27y532b" --quiet_build
+if [ $TRAVIS ]; then
+   passwordCommand="-login_password j4K7CK4oM49ZA27y532b"
+else
+   passwordCommand="--live"
+fi
+
+# Clear out the integrationTestResults folder
+rm -rf integrationTestResults/*
+
+"Heyzap/Pods/Subliminal/Supporting Files/CI/subliminal-test" -workspace "Heyzap/Heyzap.xcworkspace" -sim_device 'iPhone Retina (3.5-inch)' -output integrationTestResults $passwordCommand --quiet_build
