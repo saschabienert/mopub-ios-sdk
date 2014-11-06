9bf6d6eb5de4b4463f65ecbd280841f4bfa63c80
Maximilian Tagher <feedback.tagher@gmail.com>
2014-11-05 16:19:22 -0800

    Fix Native Ads click URLs
    Previously I wasn't replacing the values like {IDFA} which caused the click URLs to be invalid. This broke opening the URLs on iOS 5, and presumably click tracking as well.

---
+++ Heyzap/Heyzap/HZNSURLUtils.h
@@ -0,0 +1,23 @@
+//
+//  HZNSURLUtils.h
+//  Heyzap
+//
+//  Created by Maximilian Tagher on 11/5/14.
+//  Copyright (c) 2014 Heyzap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HZNSURLUtils : NSObject
+
+/**
+ *  Substitutes placeholder values like {IDFA} with their appropriate values. You *must* run this on click URLs before trying to create `NSURL` values, otherwise the URL will be invalid and will be `nil`.
+ *
+ *  @param urlString    A string value of the URL to replace values in.
+ *  @param impressionID The impression ID for the ad.
+ *
+ *  @return A URL without {placeholders}.
+ */
++ (NSString *)substituteGetParams:(NSString *)urlString impressionID:(NSString *)impressionID;
+
+@end
