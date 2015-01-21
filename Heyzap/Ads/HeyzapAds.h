54cbed44cbf05230ddc22ccc4467d7956b91eeb8
Maximilian Tagher <feedback.tagher@gmail.com>
2015-01-21 15:19:33 -0800

    Bump version to 8.0.0 to enable new video blending approach.

--- Heyzap/Ads/HeyzapAds.h
+++ Heyzap/Ads/HeyzapAds.h
@@ -46,7 +46,7 @@
 #define NS_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
 #endif
 
-#define SDK_VERSION @"7.1.0"
+#define SDK_VERSION @"8.0.0"
 
 typedef NS_ENUM(NSUInteger, HZAdOptions) {
     HZAdOptionsNone = 0 << 0,
