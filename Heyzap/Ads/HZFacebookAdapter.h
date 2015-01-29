579faca6ed24bd2a498cc2a86d59472761076f19
Maximilian Tagher <feedback.tagher@gmail.com>
2015-01-28 18:44:01 -0800

    Revert "Merge pull request #77 from Heyzap/revert-66-fb-network"
    This reverts commit 295ab687d53c65f07a601e5cb19c94f74f682c30, reversing
    changes made to 5c5f92a739f27c8b0d205e86296df39dfddd4863.

---
+++ Heyzap/Ads/HZFacebookAdapter.h
@@ -0,0 +1,14 @@
+//
+//  HZFacebookAdapter.h
+//  Heyzap
+//
+//  Created by David Stumm on 12/19/14.
+//  Copyright (c) 2014 Heyzap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HZBaseAdapter.h"
+
+@interface HZFacebookAdapter : HZBaseAdapter 
+
+@end
