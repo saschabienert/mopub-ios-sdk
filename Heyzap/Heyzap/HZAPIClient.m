bbfd7f52fcd3ef3e2b185a07933b8a0d04b80174
Maximilian Tagher <feedback.tagher@gmail.com>
2015-04-20 12:05:03 -0700

    Use dispatch_once to only create default parameter dictionary once.

--- Heyzap/Heyzap/HZAPIClient.m
+++ Heyzap/Heyzap/HZAPIClient.m
@@ -40,41 +40,51 @@ NSString * const HZAPIClientDidSendRequestNotification = @"HZAPIClientDidSendReq
     return _sharedClient;
 }
 
++ (NSMutableDictionary *)defaultParams {
+    // Profiling revealed this to be mildly expensive. The values never change so dispatch_once is a good optimization.
+    static NSDictionary *defaultParams = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        
+        NSString *deviceFormFactor;
+        if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
+            deviceFormFactor = @"tablet";
+        } else {
+            deviceFormFactor = @"phone";
+        }
+        
+        NSString *versionString = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleVersionKey];
+        versionString = versionString ?: @"";
+        
+        NSString *publisherID = [HZUtils publisherID] ?: @"";
+        
+        NSMutableDictionary *params = [@{@"publisher_id": publisherID,
+                                         @"publisher_sdk_key": publisherID,
+                                         @"device_id": [HZUtils deviceID],
+                                         @"app_bundle_id": [[NSBundle mainBundle] bundleIdentifier],
+                                         @"app_version": versionString,
+                                         @"device_form_factor": deviceFormFactor,
+                                         @"platform": @"iphone",
+                                         @"sdk_platform": @"iphone",
+                                         @"sdk_version": SDK_VERSION,
+                                         @"ios_version": [UIDevice currentDevice].systemVersion,
+                                         @"os_version": [UIDevice currentDevice].systemVersion,
+                                         @"device_type": [HZAvailability platform],
+                                         @"advertising_id" : [HZUtils deviceID],
+                                         } mutableCopy];
+        
+        [params addEntriesFromDictionary:[[HZDevice currentDevice] HZIdentifierDictionary]];
+        defaultParams = params;
+    });
+    
+    return [defaultParams mutableCopy];
+}
+
 + (NSMutableDictionary *) defaultParamsWithDictionary: (NSDictionary *) dictionary {
-    
-    NSString *deviceFormFactor;
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad) {
-        deviceFormFactor = @"tablet";
-    } else {
-        deviceFormFactor = @"phone";
-    }
-    
-    NSString *versionString = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString*)kCFBundleVersionKey];
-    versionString = versionString ?: @"";
-    
-    NSString *publisherID = [HZUtils publisherID] ?: @"";
-    
-    NSMutableDictionary *params = [@{@"publisher_id": publisherID,
-                                     @"publisher_sdk_key": publisherID,
-                                     @"device_id": [HZUtils deviceID],
-                                     @"app_bundle_id": [[NSBundle mainBundle] bundleIdentifier],
-                                     @"app_version": versionString,
-                                     @"device_form_factor": deviceFormFactor,
-                                     @"platform": @"iphone",
-                                     @"sdk_platform": @"iphone",
-                                     @"sdk_version": SDK_VERSION,
-                                     @"ios_version": [UIDevice currentDevice].systemVersion,
-                                     @"os_version": [UIDevice currentDevice].systemVersion,
-                                     @"device_type": [HZAvailability platform],
-                                     @"advertising_id" : [HZUtils deviceID],
-                                   } mutableCopy];
-    
+    NSMutableDictionary *params = [self defaultParams];
     if (dictionary) {
         [params addEntriesFromDictionary: dictionary];
     }
-    
-    [params addEntriesFromDictionary:[[HZDevice currentDevice] HZIdentifierDictionary]];
-    
     return params;
 }
 
