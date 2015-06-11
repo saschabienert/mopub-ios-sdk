6cb6d105622f4bb89b70680814c4e0b5293482be
Monroe Ekilah <monroe@heyzap.com>
2015-06-10 17:34:51 -0700

    updates comment - bad grammar

--- Heyzap/Ads/HeyzapAds.h
+++ Heyzap/Ads/HeyzapAds.h
@@ -257,7 +257,7 @@ extern NSString * const kHeyzapAdsCustomPublisherDataRefreshedNotification;
 /**
  * Returns a dictionary of publisher-settable data.
  
- * Note: This data is cached, so it will usually be available at app launch. It is updated via a network call that is made when `[HeyzapAds startWithPublisherId:]` (or one of it's related methods) is called. If you want to guarantee that the data has been refreshed, only use it after receiving an NSNotification with name=`kHeyzapAdsCustomPublisherDataRefreshedNotification`. The userInfo passed with the notification will be the same NSDictionary you can receive with this method call. 
+ * Note: This data is cached, so it will usually be available at app launch. It is updated via a network call that is made when `[HeyzapAds startWithPublisherId:]` (or one of its related methods) is called. If you want to guarantee that the data has been refreshed, only use it after receiving an NSNotification with name=`kHeyzapAdsCustomPublisherDataRefreshedNotification`. The userInfo passed with the notification will be the same NSDictionary you can receive with this method call. 
  */
 + (NSDictionary *) getCustomPublisherData;
 
