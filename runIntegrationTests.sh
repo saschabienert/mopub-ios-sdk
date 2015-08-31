e9460fe630f7d0c55852b0d92cb41f9599603e67
Maximilian Tagher <feedback.tagher@gmail.com>
2015-08-31 13:23:04 -0700

    Remove integration tests
    * Subliminal is no longer supported
    * We haven't run these tests in ages

--- runIntegrationTests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Clear out the integrationTestResults folder
-rm -rf integrationTestResults/*
-
-"Heyzap/Pods/Subliminal/Supporting Files/CI/subliminal-test" -workspace "Heyzap/Heyzap.xcworkspace" -sim_device 'iPhone 5' -output integrationTestResults --quiet_build
