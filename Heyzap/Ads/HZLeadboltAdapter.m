a6749d7830a7266e68aa68396caa9e6745ece536
Monroe Ekilah <monroe@heyzap.com>
2015-09-08 16:58:08 -0700

    removes duplicate leadbolt initialization callback

--- Heyzap/Ads/HZLeadboltAdapter.m
+++ Heyzap/Ads/HZLeadboltAdapter.m
@@ -69,8 +69,6 @@ NSString * const kHZLeadboltIncentivizedModule = @"video";
     HZDLog(@"Initializing Leadbolt with App API Key: %@",self.appAPIKey);
     [HZAppTracker startSession:self.appAPIKey]; // NB: Leadbolt must be started after registering for NSNotifications per docs.
     
-    [[HeyzapMediation sharedInstance] sendNetworkCallback:HZNetworkCallbackInitialized forNetwork:[self name]];
-    
     return nil;
 }
 
