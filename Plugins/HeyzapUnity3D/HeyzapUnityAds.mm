d72bfd4fb3732a3c53f4901110d60788cd32a137
Maximilian Tagher <feedback.tagher@gmail.com>
2015-09-24 13:29:48 -0700

    Add logging to Chartboost direct calls

--- Plugins/HeyzapUnity3D/HeyzapUnityAds.mm
+++ Plugins/HeyzapUnity3D/HeyzapUnityAds.mm
@@ -310,12 +310,13 @@ extern "C" {
     // Since I want to call it recursively, I immediately convert to an `NSString *` in `hz_fetch_chartboost_for_location`
     void hz_fetch_chartboost_for_location_objc(NSString *location) {
         if (!hz_chartboost_enabled()) {
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"Chartboost not enabled; in 0.25 seconds");
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.25 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
                 hz_fetch_chartboost_for_location_objc(location);
             });
             return;
         }
-        
+        NSLog(@"Caching Chartboost interstitial for location: %@",location);
         [HZUnityAdapterChartboostProxy cacheInterstitial:location];
     }
     
@@ -327,9 +328,12 @@ extern "C" {
     bool hz_chartboost_is_available_for_location(const char *location) {
         NSString *nsLocation = [NSString stringWithUTF8String:location];
         if (!hz_chartboost_enabled()) {
+            NSLog(@"Chartboost ad is not available because it is not enabled");
             return NO;
         }
-        return [HZUnityAdapterChartboostProxy hasInterstitial:nsLocation];
+        const BOOL hasAd = [HZUnityAdapterChartboostProxy hasInterstitial:nsLocation];
+        NSLog(@"Chartboost says it has an ad = %i",hasAd);
+        return hasAd;
     }
     
     void hz_show_chartboost_for_location(const char *location) {
@@ -339,7 +343,7 @@ extern "C" {
             NSLog(@"Chartboost not enabled yet; not able to show ad.");
             return;
         }
-        
+        NSLog(@"Requesting Chartboost show interstitial for location: %@",nsLocation);
         [HZUnityAdapterChartboostProxy showInterstitial:nsLocation];
     }
 }
