f2cc18ff24866db323230b324671964d5960cfcb
Maximilian Tagher <feedback.tagher@gmail.com>
2015-11-30 17:02:36 -0800

    Respond to comments for HZAbstractAdMobNativeAdAdapter's runtime-subclassing

--- Heyzap/Ads/HZAbstractAdMobNativeAdAdapter.m
+++ Heyzap/Ads/HZAbstractAdMobNativeAdAdapter.m
@@ -20,14 +20,19 @@
 
 @implementation HZAbstractAdMobNativeAdAdapter
 
+// Each AdMob native ad has its own UIView subclass (e.g. GADNativeContentAdView).
+// We need to know when that view is on-screen. To do this, I subclass the AdMob class and override `didMoveToWindow`
+// When the view is added to the window, our subclass sends `admobWrapperMovedToWindow` to its delegate, the adapter (see method below)
+// At this point the adapter uses a timer to continually check if the AdMob ad is actually visible on screen. If so, an impression is reported.
 + (Class)wrapperViewSubclass:(NSString *)subclassName forSuperclass:(NSString *)superclassName {
     Class wrapperSuperClass = NSClassFromString(superclassName);
     Class subclass = objc_allocateClassPair(wrapperSuperClass, [subclassName UTF8String], 0);
     
+    // class_addIvar line from: https://www.mikeash.com/pyblog/friday-qa-2010-11-6-creating-classes-at-runtime-in-objective-c.html
     class_addIvar(subclass, "_heyzapDelegate", sizeof(id), rint(log2(sizeof(id))), @encode(id));
     
     IMP imp = imp_implementationWithBlock(^(id theSelf){
-        class_getSuperclass(subclass);
+        // AdMob requires calling super for all overridden methods
         struct objc_super super_data = { theSelf, wrapperSuperClass };
         objc_msgSendSuper(&super_data, @selector(didMoveToWindow));
         
